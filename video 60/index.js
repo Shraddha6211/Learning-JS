0e0497a8f401668682d654d00396f2188fe3c357
Shraddha Tiwari <121687112+Shraddha6211@users.noreply.github.com>
2025-01-12 23:26:46 +0545

    Strings exercise from CWH
    learnt about string and some of its commands

---
+++ video 60/index.js
@@ -0,0 +1,12 @@
+console.log("har\"".length)
+//This will print number of characters as 4 only 
+
+//Explore the includes, startswith and ends with functions of a string
+//Write a program to convert a given string to lowercase
+let a="Shraddhu babe"
+console.log(a.toLowerCase())
+//Extract the amount out of this string "Please give me Rs 1000"
+let sentence="Please give me Rs 1000";
+console.log(sentence.slice(18))
+//Try to change fourth character of the given string were you able to do it?
+console.log(sentence.replace("s","q"))
