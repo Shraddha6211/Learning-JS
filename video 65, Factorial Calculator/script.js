72cc9fa64e28697151bcf6e15d802766f643623c
Shraddha Tiwari <121687112+Shraddha6211@users.noreply.github.com>
2025-01-16 22:56:40 +0545

    video 65
    factor calculator

---
+++ video 65, Factorial Calculator/script.js
@@ -0,0 +1,23 @@
+// Factorial Calculator using For loop and Reduce array function
+// Using For loop
+let num= parseFloat(prompt("Enter a number:"));
+let arr=[];
+for(let i=1; i<=num; i++){
+    arr[i-1]=i;
+}
+let Factorial=1;
+for(let i=0; i<arr.length;i++){
+    Factorial*=arr[i];
+}
+console.log(`The factorial of ${num} is ${Factorial}.`);
+//Using Reduce array function
+// let num= parseFloat(prompt("Enter a number:"));
+// let arr=[];
+// for(let i=1; i<=num; i++){
+//     arr[i-1]=i;
+// }
+// let Factorial=arr.reduce((prev, curr)=>{
+//     return prev*curr;
+// })
+// console.log(Factorial);
+
